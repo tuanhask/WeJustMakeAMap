a39d7d86d497be6226f216f7be0a5da1b9d5e8ed
Tran Minh Hieu <hieutm@fimo.edu.vn>
2019-11-15 08:46:06 +0700

    alpha release

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ var iconMarkerEl = document.createElement("div");
 iconMarkerEl.innerHTML = "<div class='marker-arrow'></div>" +
   "<div class='marker-pulse'></div>";
 
-map.addControl(new PeliasGeocoder({
+var geocoder = new PeliasGeocoder({
   params: {'access-token': 'P4QzOCt4wWfPKfuPuMF1UQSupLKLsO7ZC4DBRF6GAoC0AcaSAKRvD4v2vix948q7'},
   flyTo: 'hybrid',
   wof: true,
@@ -13,9 +13,22 @@ map.addControl(new PeliasGeocoder({
     icon: iconMarkerEl,
     multiple: false
   }
-}));
+});
+map.addControl(geocoder);
 
-var detailFeature = document.createElement("div");
+document.getElementById('close-detail-button').onclick = function(e) {
+  document.getElementById('detail-feature').style.display = "none";
+};
+
 function showDetailFeature(feature) {
   console.log(feature);
+  document.getElementById('detail-feature').style.display = "block";
+  let featureName = document.getElementById("feature-name");
+  featureName.innerHTML = feature.properties.name + "<br>"
+                        + "<div class='feature-type'>" + feature.geometry.type + "</div>";
+  let featureCoordinates = document.getElementById("feature-coordinates");
+  featureCoordinates.innerHTML = '<i class="fas fa-compass"></i>  ' + feature.geometry.coordinates[0] + ", " + feature.geometry.coordinates[1];
+  let featureLocation = document.getElementById("feature-location");
+  featureLocation.innerHTML = '<i class="fas fa-map"></i>  ' + feature.properties.street + ", " + feature.properties.county + ", "
+                              + feature.properties.region + ", " + feature.properties.country;
 }
